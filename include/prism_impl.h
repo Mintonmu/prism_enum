a513be03cda2bb121520e97138e022fff1c87dc4
mintonmu <mintonmu@outlook.com>
2026-04-11 22:15:57 +0800

    update variable name

--- include/prism_impl.h
+++ include/prism_impl.h
@@ -13,7 +13,7 @@ struct is_scoped_enum<T, true>
 };
 
 template <typename T>
-using is_scoped_enum_t = typename is_scoped_enum<T>::value;
+inline constexpr bool is_scoped_enum_v = is_scoped_enum<T>::value;
 
 template <typename T, bool = std::is_enum_v<T>>
 struct is_unscoped_enum : std::false_type {};
@@ -24,7 +24,7 @@ struct is_unscoped_enum<T, true>
 };
 
 template <typename T>
-using is_unscoped_enum_t = typename is_unscoped_enum<T>::value;
+inline constexpr bool is_unscoped_enum_v = is_unscoped_enum<T>::value;
 } // namespace detail
 } // namespace prism_enum
 #endif // __PRISM_IMPL_H__
